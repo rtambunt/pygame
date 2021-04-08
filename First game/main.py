0798751160e019480286bfd7c60651c45cc41c9f
rtambunt <rtambunting@gmail.com>
2021-04-08 09:45:01 -0700

    Ball collision

--- First game/main.py
+++ First game/main.py
@@ -14,8 +14,8 @@ YELLOW = (255, 255, 0)
 
 BORDER = pygame.Rect(WIDTH // 2 - 5, 0, 10, HEIGHT)
 
-BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Grenade+1.mp3'))
-BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Gun+Silencer.mp3'))
+BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('First game/Assets', 'Grenade+1.mp3'))
+BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('First game/Assets', 'Gun+Silencer.mp3'))
 
 HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
 WINNER_FONT = pygame.font.SysFont('comicsans', 100)
@@ -29,13 +29,13 @@ SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 YELLOW_HIT = pygame.USEREVENT + 1
 RED_HIT = pygame.USEREVENT + 2
 
-YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('First Game/Assets', 'spaceship_yellow.png'))
 YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 
-RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('First Game/Assets', 'spaceship_red.png'))
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
-SPACE = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'space.png')), (WIDTH, HEIGHT))
+SPACE = pygame.transform.scale(pygame.image.load(os.path.join('First Game/Assets', 'space.png')), (WIDTH, HEIGHT))
 
 def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
     WIN.blit(SPACE, (0, 0))
--- Pong/main.py
+++ Pong/main.py
@@ -13,35 +13,54 @@ BORDER = pygame.Rect(WIDTH // 2 - 2, 0, 4, HEIGHT)
 PADDLE_WIDTH, PADDLE_HEIGHT = 5, 50
 PADDLE_VEL = 5
 
+# Ball 
+BALL_WIDTH, BALL_HEIGHT = 7, 7
+ball_vel_x = -5
+ball_vel_y = 0
+
 # Colors (RGB)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 # Functions
 
-def draw_window(paddle1, paddle2):
+def draw_window(paddle1, paddle2, ball):
     WIN.fill(BLACK)
     pygame.draw.rect(WIN, WHITE, BORDER)
     pygame.draw.rect(WIN, WHITE, paddle1)
     pygame.draw.rect(WIN, WHITE, paddle2)
+    pygame.draw.rect(WIN, WHITE, ball) 
     pygame.display.update()
 
 def paddle1_handle_movement(keys_pressed, paddle1):
-    if keys_pressed[pygame.K_w]:
+    if keys_pressed[pygame.K_w] and paddle1.y > 0:
         paddle1.y -= PADDLE_VEL
-    if keys_pressed[pygame.K_s]:
+    if keys_pressed[pygame.K_s] and paddle1.y + PADDLE_HEIGHT < HEIGHT:
         paddle1.y += PADDLE_VEL
 
 def paddle2_handle_movement(keys_pressed, paddle2):
-    if keys_pressed[pygame.K_UP]:
+    if keys_pressed[pygame.K_UP] and paddle2.y > 0:
         paddle2.y -= PADDLE_VEL
-    if keys_pressed[pygame.K_DOWN]:
+    if keys_pressed[pygame.K_DOWN] and paddle2.y + PADDLE_HEIGHT < HEIGHT:
         paddle2.y += PADDLE_VEL
 
-def main():
-    paddle1 = pygame.Rect(100, 226, PADDLE_WIDTH, PADDLE_HEIGHT)
-    paddle2 = pygame.Rect(800, 226, PADDLE_WIDTH, PADDLE_HEIGHT)
+def ball_handle_movement(paddle1, paddle2, ball):
+    global ball_vel_x, ball_vel_y
 
+    ball.x += ball_vel_x
+    ball.y += ball_vel_y
+
+    if ball.colliderect(paddle1) or ball.colliderect(paddle2):
+        ball_vel_x *= -1
+       
+    if ball.top <= 0 or ball.bottom >= HEIGHT:
+        ball_vel_y *= -1
+
+def main():
+    paddle1 = pygame.Rect(100, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    paddle2 = pygame.Rect(WIDTH - 100, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    ball = pygame.Rect(WIDTH // 2 - 20, HEIGHT // 2 - BALL_HEIGHT // 2, BALL_WIDTH, BALL_HEIGHT)
+    
     run = True
     clock = pygame.time.Clock()
 
@@ -54,8 +73,8 @@ def main():
         keys_pressed = pygame.key.get_pressed()
         paddle1_handle_movement(keys_pressed, paddle1)
         paddle2_handle_movement(keys_pressed, paddle2)
-
-        draw_window(paddle1, paddle2)
+        ball_handle_movement(paddle1, paddle2, ball)
+        draw_window(paddle1, paddle2, ball)
 
     pygame.quit()
 
